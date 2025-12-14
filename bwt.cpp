804cdef9d9fbcb6fab3a40abc7b0411cd37fb954
WilliamAdamsWAG <williamadams.aurora@gmail.com>
2025-12-14 16:23:14 +0300

    style, fix, docs

--- bwt.cpp
+++ bwt.cpp
@@ -7,55 +7,54 @@ using namespace std;
 
 namespace bwt {
     void shiftString(std::string& str) {
-        int shift = 1;
         if (str.empty()) {
-            return;
-        }
-        int n = str.length();
-        shift %= n;
-        if (shift < 0) {
-            shift += n;
+            return;  // Если строка пустая
         }
 
-        std::string temp = str.substr(n - shift);
-        str.erase(n - shift);
-        str.insert(0, temp);
+        std::string temp = str.substr(str.length() - 1);  // всё кроме первого элемента
+        str.erase(str.length() - 1);  // удаляем из строки всё кроме первого символа
+        str.insert(0, temp);  // Вставляем удалённое в начало
     }
 
     void encode(const string& s, string& buf, int& index)
     {
-        vector<string> shifts(s.length());
-        string cur = s;
+        vector<string> shifts(s.length());  // Таблица сдвигов
+        string cur = s; // Текущая обрабатываемая строка
 
+        // Заполняем таблицу сдвигов
         for(int i = 0; i != (int)s.length(); i++)
         {
-            shifts[i] = cur;
-            shiftString(cur);
+            shifts[i] = cur;  // Добавляем сдвиг
+            shiftString(cur); // Сдвигаем строку
         }
 
+        // Сортируем таблицу сдвигов
         sort(shifts.begin(), shifts.end());
 
+        // Собираем результат и ищем индекс
         for(int i = 0; i != (int)s.length(); i++)
         {
             if (shifts[i] == s) {
-                index = i;
+                index = i; // Индекс исходного значения
             }
+            // Добавляем к результату последний символ каждой строки в таблице сдвигов
             buf = buf + shifts[i].back();
         }
     }
 
     void decode(const string& s, string& buf, int& index) {
-        vector<string> shifts(s.length());
+        vector<string> shifts(s.length());  // Таблица сдвигов
 
         for(int i = 0; i != (int)s.length(); i++)
         {
             for(int j = 0; j != (int)s.length(); j++)
             {
+                // вставляем в начало строки shifts[j] символ s[j]
                 shifts[j].insert(0, std::string(1, s[j]));
             }
-            sort(shifts.begin(), shifts.end());
+            sort(shifts.begin(), shifts.end()); // сортируем результат
         }
 
-        buf = shifts[index];
+        buf = shifts[index];  // Берём из таблицы сдвигов значение по индексу
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
 
 #include "bwt.h"
@@ -9,16 +7,19 @@ using namespace std;
 
 int main()
 {
-    string x = ".BANANA.";
+    string x;
+    
     string result_of_encode = "";
     int i;
     string result_of_decode = "";
+
+    cin >> x;
     
     bwt::encode(x, result_of_encode, i);
     bwt::decode(result_of_encode, result_of_decode, i);
 
-    cout << result_of_encode << i << endl;
-    cout << result_of_decode << endl;
+    cout << "Результат кодирования: " << result_of_encode << endl;
+    cout << "Результат декодирования: " << result_of_decode << endl;
 
     return 0;
 }
